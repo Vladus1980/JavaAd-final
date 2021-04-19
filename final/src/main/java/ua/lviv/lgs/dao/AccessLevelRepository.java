7610d9f9910b11000ada7133c40a9471c09bbd88
DESKTOP-UQU6I9G\Vlad <valdus1980@gmail.com>
2021-04-19 18:25:32 +0300

    commit2

--- final/src/main/java/ua/lviv/lgs/dao/AccessLevelRepository.java
+++ final/src/main/java/ua/lviv/lgs/dao/AccessLevelRepository.java
@@ -1,11 +1,17 @@
 package ua.lviv.lgs.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import ua.lviv.lgs.domain.AccessLevel;
 
 @Repository
 public interface AccessLevelRepository extends JpaRepository<AccessLevel, Integer> {
+	
+	@Query("select a.role from AccesslEVEL a, user b where b.userName=? and a.userId=b.userId")
+	public List<String> findRolesByUserName(String username);
 
 }
