76a63921881b012c11ed944ff050f980bdbe25ac
DESKTOP-UQU6I9G\Vlad <valdus1980@gmail.com>
2021-04-19 18:25:46 +0300

    commit 3

--- final/src/main/java/ua/lviv/lgs/security/CustomUserDetailsService.java
+++ final/src/main/java/ua/lviv/lgs/security/CustomUserDetailsService.java
@@ -1,7 +1,10 @@
 package ua.lviv.lgs.security;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.security.acls.model.NotFoundException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,9 +20,15 @@ public class CustomUserDetailsService implements UserDetailsService{
 	private AccessLevelRepository accessLevelRepository;
 
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		
-		return null;
+		
+		Optional<User> userOptional = userRepository.findByEmail(email);
+		if(userOptional.isPresent()) {
+			List<String> accessLevel = accessLevelRepository.findRolesByUserName(email);
+			return new CustomUserDetails(userOptional.get(), accessLevel);
+		}
+		throw new NotFoundException("Немає користувача з логіном: "+ email);
 	}
 
 	
