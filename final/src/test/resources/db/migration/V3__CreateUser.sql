6d6386b92746c0cadde6b9f27c6b7d8303cbf421
DESKTOP-UQU6I9G\Vlad <valdus1980@gmail.com>
2021-05-07 11:15:39 +0300

    merge i18n to master

---
+++ final/src/test/resources/db/migration/V3__CreateUser.sql
@@ -0,0 +1,5 @@
+INSERT INTO user VALUES
+(2, 'ZZ', 'ZZ', 'valdus1980@gmail.com', '$2a$08$GqFnDNRtlw3Ib1bdM7BzSesSALwiMOFSwSewHNflrTB5IDaug6AQ6', TRUE, NULL);
+
+INSERT INTO access_level VALUES
+(2, 'USER');
